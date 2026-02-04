de7985bdd19ab5b27bf41b803d0ce6083709505f
Balaraman <balaraman.e@avantaifintech.com>
2026-02-04 13:38:32 +0530

    server ignore

--- server.js
+++ server.js
@@ -1,27 +1,33 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
+
 const app = express();
 
-// ✅ ADD HERE (top, before routes)
+/* ================= MIDDLEWARE ================= */
+app.use(cors());
 app.use(express.json({ strict: true }));
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use(cors()); // 👈 ALLOW ALL ORIGINS
-app.use(express.json());
-app.use(express.json());
-
-// 👇 REQUIRED to access uploaded images
+// Static uploads
 app.use('/uploads', express.static('uploads'));
 
+/* ================= ROUTES ================= */
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/products', require('./routes/product'));
 app.use('/api/inventory', require('./routes/inventoryRoutes'));
 
+/* ================= HTTPS SERVER ================= */
+const sslOptions = {
+  key: fs.readFileSync('/home/ec2-user/ssl/key.pem'),
+  cert: fs.readFileSync('/home/ec2-user/ssl/cert.pem'),
+};
 
-app.listen(3000, () => console.log('Server running on port 3000'));
-
+https.createServer(sslOptions, app).listen(443, () => {
+  console.log('HTTPS Server running on https://18.60.129.193');
+});
 
 
 
