958367562bcd540e81d09160a511ab3d3d6c95a9
Balaraman <balaraman.e@avantaifintech.com>
2026-02-06 17:03:43 +0530

    url added login

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true }));
 const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true }));
 
 /* ================= MIDDLEWARE ================= */
 app.use(cors());
